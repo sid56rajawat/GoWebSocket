fe0c333d76360e2bfc8fa5ee10be3d42abcd825d
SSIDDHARTHAS <srajawat@avaya.com>
2024-10-24 18:20:53 +0530

    add: handle websocket and broadcast functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sid56rajawat/websocket-demo
 
 go 1.23.2
+
+require golang.org/x/net v0.30.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	connections map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		connections: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client", ws.RemoteAddr())
+
+	s.connections[ws] = true
+
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Println(string(msg))
+
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.connections {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
 }
